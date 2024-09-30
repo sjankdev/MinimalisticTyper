c0d75e91a5aa72c285cf39c49e1d53bc0234733f
sjankdev <stefanjanko.dev@gmail.com>
2024-09-30 16:19:59 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  text: {
+    fontSize: 24,
+    color: '#333',
+  },
+});
+
+export default App;
