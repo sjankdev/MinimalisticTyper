a6a2ef2113d5f61ca318ad5c9005e6cf634181c0
sjankdev <stefanjanko.dev@gmail.com>
2024-09-30 18:06:13 +0200

    Removing headers and top bar

--- App.tsx
+++ App.tsx
@@ -3,18 +3,29 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainPage from './MainPage';
 import TypingPage from './TypingPage';
+import { StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <StatusBar hidden={true} />
       <Stack.Navigator initialRouteName="Main">
-        <Stack.Screen name="Main" component={MainPage} />
-        <Stack.Screen name="Typing" component={TypingPage} />
+        <Stack.Screen
+          name="Main"
+          component={MainPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Typing"
+          component={TypingPage}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
 
 export default App;
